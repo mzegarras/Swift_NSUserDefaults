85ff3fabb1b5f40421ca376da24f67c634c64d25
manuelzegarra <mzegarra@gmail.com>
2015-09-08 23:16:43 -0500

    Initial Commit

---
+++ preferencias/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  preferencias
+//
+//  Created by Manuel Zegarra on 8/09/15.
+//  Copyright (c) 2015 M-Sonic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
