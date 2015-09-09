77eef2a479d680d1023d6a58543a7434acbe7640
manuelzegarra <mzegarra@gmail.com>
2015-09-09 00:00:18 -0500

    NSUserDefaults Demo

--- preferencias/ViewController.swift
+++ preferencias/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var txtValor: UITextField!
+    
+    @IBOutlet weak var lblValor: UILabel!
+    
+    let userDefaults = NSUserDefaults.standardUserDefaults()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var valor = userDefaults.integerForKey("PARAM");
+        
+        if valor==0 {
+            lblValor.text =  "Sin valor";
+        }else{
+            lblValor.text =  "\(valor)";
+        }
+        
+        
+        
+        
+        txtValor.keyboardType = UIKeyboardType.NumberPad
+        txtValor.returnKeyType = UIReturnKeyType.Done
+        
+        txtValor.delegate = self;
+        
     }
+    
+  
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btnClick(sender: UIButton) {
+        
+        var valor = txtValor.text.toInt()
+        
+        userDefaults.setInteger(valor!, forKey: "PARAM")
+        
+        
     }
-
-
+    
+    
+    @IBAction func btnBorrar(sender: UIButton) {
+        userDefaults.removeObjectForKey("PARAM")
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        //self.view.endEditing(true)
+        textField.resignFirstResponder();
+        return false
+    }
+    
 }
 
